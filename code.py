2473fb6db7d32baccf2ca5120d63a02799ada218
Kevin Clement <kevin.clement@gmail.com>
2026-08-10 01:02:27 -0700

    led working

--- code.py
+++ code.py
@@ -83,70 +83,20 @@ while True:
     if press_start_time is None and click_count > 0 and (time.monotonic() - last_click_time) > DOUBLE_CLICK_MAX_DELAY:
         if click_count == 1 and not long_press_reported:
             print("Single click detected!")
+            if RECORDING:
+                RECORDING = False
+                print("STOPPING recording")
+            else:
+                RECORDING = True
+                print("STARTING recording")
+                # Send F12 for RECORD PUNCH (start)
+                # alternative is usb.COMMAND usb.SPACE
+                # TODO: restore before going live
+                #kbd.send(usb.F12)            
+            led.value = LED_ON if RECORDING else LED_OFF
         elif click_count == 2:
             print("Double click detected!")
         click_count = 0
 
     time.sleep(0.01)  # Small delay to reduce CPU usage
-    # switch.update()
     
-    # # Detect a press event
-    # if switch.fell:
-    #     now = time.monotonic()
-    #     if now - last_click_time <= DOUBLE_CLICK_MAX_DELAY:
-    #         click_count += 1
-    #     else:
-    #         click_count = 1  # reset if too much time passed
-    #     last_click_time = now
-
-    # # Detect long press while holding
-    # if switch.value and press_start_time is not None:
-    #     if not long_press_reported and (time.monotonic() - press_start_time) >= LONG_PRESS_TIME:
-    #         print("Long press detected!")
-    #         long_press_reported = True
-    #         click_count = 0  # Cancel click counting if it's a long press
-
-    # # Detect button release
-    # if switch.rose:
-    #     press_start_time = None
-
-    # # Check if enough time has passed since last click to decide
-    # if click_count > 0 and (time.monotonic() - last_click_time) > DOUBLE_CLICK_MAX_DELAY:
-    #     if click_count == 1:
-    #         print("Single click detected!")
-
-    #         # # SINGLE CLICK: Send space bar for record/stop
-    #         # if RECORDING:
-    #         #     RECORDING = False
-    #         #     print("STOPPING recording")
-    #         # else:
-    #         #     RECORDING = True
-    #         #     print("STARTING recording")                            
-    #         #     # Send F12 for RECORD PUNCH (start)
-    #         #     # alternative is usb.COMMAND usb.SPACE
-    #         #     # TODO: uncomment before going live
-    #         #     #kbd.send(usb.F12)    
-    #         led.value = LED_ON if RECORDING else LED_OFF
-    #     elif click_count == 2:
-    #         print("Double click detected!")
-    #     click_count = 0  # reset after reporting
-
-    # time.sleep(0.01)  # Small delay to reduce CPU usage
-
-    # if not btn1.value:
-    #     # Debounce
-    #     time.sleep(0.3)
-    #     while not btn1.value:
-    #         pass
-    #     if RECORDING:
-    #         RECORDING = False
-    #         print("stoping recording")
-    #     else:
-    #         RECORDING = True
-    #         print("starting recording")
-    #         # Send F12 for RECORD PUNCH (start)
-    #         # alternative is usb.COMMAND usb.SPACE
-    #         kbd.send(usb.F12)            
-    #     led.value = LED_ON if RECORDING else LED_OFF
-    #     # led.value = LED_ON
-    # time.sleep(0.2)  # Write your code here :-)
